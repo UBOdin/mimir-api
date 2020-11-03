UBOdin/mimir-api

commit 454b480a52168c139a2236cc52849ab0ef1f1fc9

Fix ExplainSpec and other misc tweaks for geocoding
The big problem is that in *some* cases, spark's optimizer can push a filter down below the mimir construct that builds ROWIDs.  This is... problematic, since it shifts the ROWID for all records past the first record that fails a filter.  This patch (among other things) changes things so that ROWIDs are hard-assigned to any parquet file created by mimir.

- Fixed parsing String Keys for Caveats in JSON format
- Catalog's stageAndPut now adds rowids if staging requires an intermediate dataframe (fixes ExplainSpec)
- Geocoding lens now only annotates records where the coordinates are unknown
- typo in the loggerutils package name
- Test cases now use simple prettyprinting (works w/ Bloop and SBT)
- Slightly extended ExplainSpec

build.sbt
@@ -30,7 +30,7 @@ excludeDependencies ++= Seq(
 // Custom Dependencies
 libraryDependencies ++= Seq(
   // Mimir
-  "org.mimirdb"                   %% "mimir-caveats"             % "0.2.6" excludeAll( ExclusionRule("com.fasterxml.jackson.core"), ExclusionRule(organization ="org.apache.httpcomponents", name="httpcore")),
+  "org.mimirdb"                   %% "mimir-caveats"             % "0.2.7" excludeAll( ExclusionRule("com.fasterxml.jackson.core"), ExclusionRule(organization ="org.apache.httpcomponents", name="httpcore")),
   // "org.mimirdb"                   %% "mimir-vizual"              % "0.1-SNAPSHOT",
 
   // API

src/main/scala/org/mimirdb/api/CaveatFormat.scala
@@ -17,7 +17,7 @@ object CaveatFormat
         // line, since spark annoyingly hides the non-json version from us.
         val t = DataType.fromJson(fields("dataType").toString) 
         JsSuccess(
-          Literal(SparkPrimitive.decode(fields("value"), t), t)
+          Literal(SparkPrimitive.decode(fields("value"), t))
         )
       }
     },

src/main/scala/org/mimirdb/data/Catalog.scala
@@ -12,6 +12,7 @@ import org.mimirdb.spark.PythonUDFBuilder
 import org.mimirdb.profiler.DataProfiler
 import org.mimirdb.util.ExperimentalOptions
 import org.mimirdb.util.TaskDeduplicator
+import org.mimirdb.rowids.AnnotateWithRowIds
 
 /**
  * Lazy data ingest and view management for Mimir
@@ -118,13 +119,32 @@ class Catalog(
     format: String = bulkStorageFormat,
     replaceIfExists: Boolean = true,
     properties: Map[String, JsValue] = Map.empty,
-    proposedSchema: Option[Seq[StructField]] = None
+    proposedSchema: Option[Seq[StructField]] = None,
+    includeRowId: Boolean = true
   )
   {
-    val url = staging.stage(df, format, Some(name))
+    val url = staging.stage(
+      input = (
+        if(includeRowId) { AnnotateWithRowIds(df) }
+        else { df }
+      ), 
+      format = format, 
+      nameHint = Some(name)
+    )
     put(
       name, 
-      LoadConstructor(url, format, Map(), Seq(), None, proposedSchema),
+      LoadConstructor(
+        url, 
+        format, 
+        Map(), 
+        Seq(), 
+        None, 
+        proposedSchema.map { _ ++ (
+          if(includeRowId) { 
+            Some(StructField(AnnotateWithRowIds.ATTRIBUTE, StringType))
+          } else { None }
+        )} 
+      ),
       Set(),
       replaceIfExists,
       properties = properties

src/main/scala/org/mimirdb/lenses/implementation/GeocodingLens.scala
@@ -10,13 +10,16 @@ import org.apache.spark.sql.functions.{
   size => array_size, 
   element_at, 
   when,
-  col
+  col, 
+  concat, 
+  lit
 }
 
 import org.mimirdb.lenses.Lens
 import org.mimirdb.data.Catalog
 import org.mimirdb.util.HttpUtils
 import org.mimirdb.caveats.implicits._
+import org.mimirdb.util.StringUtils
 
 case class GeocoderConfig(
   houseColumn: String,
@@ -104,7 +107,7 @@ class GeocodingLens(
                             addresses(STATE)
                           ) as COORDS
                       )
-      catalog.stageAndPut(cacheCode, newCache, cacheFormat)
+      catalog.stageAndPut(cacheCode, newCache, cacheFormat, includeRowId = true)
     } 
     return Json.toJson(config.withGeocoderAndCacheCode(geocoder, cacheCode))
   }
@@ -115,7 +118,49 @@ class GeocodingLens(
     val cache = catalog.get(config.cacheCode.get)
     val geocoder = config.geocoder
                           .getOrElse { geocoders.head._1 }
-    val coordinates = col("_2").getField(COORDS).caveat(s"A geocoder (${geocoder}) to determine this value")
+    val coordinates = col("_2").getField(COORDS)
+
+    def caveatMessage(multiplicity: String) = 
+    {
+      concat(
+        lit(s"Geocoder $geocoder provided ${multiplicity} coordinates for '"),
+        col("_2").getField(HOUSE) , lit(" "),
+        col("_2").getField(STREET), lit("; "),
+        col("_2").getField(CITY)  , lit(", "),
+        col("_2").getField(STATE) , lit("'"),
+      )      
+    }
+    val caveatKey = Seq(
+      col("_2").getField(HOUSE), 
+      col("_2").getField(STREET), 
+      col("_2").getField(CITY), 
+      col("_2").getField(STATE)
+    )
+
+    val fieldNames = input.schema.fieldNames.toSet
+
+    def buildLatOrLon(lat: Boolean) =
+    {
+      val (coord_idx, stringLatOrLon, colName) = (
+        if(lat) { (1, "latitude",  config.lat) }
+        else    { (2, "longitude", config.lon) }
+      )
+
+      when(array_size(coordinates) >= 2,
+           element_at(coordinates, coord_idx)
+              .caveatIf(
+                message = caveatMessage("multiple"),
+                family = config.cacheCode.get,
+                condition = array_size(coordinates) > 2
+              )(caveatKey:_*)
+      ).otherwise(
+        lit(null).caveat(
+                message = caveatMessage("no"), 
+                family = config.cacheCode.get
+              )(caveatKey:_*)
+      ).as(StringUtils.uniqueName(colName, fieldNames))
+    }
+    // cache.show()
 
     input.joinWith(
       cache,
@@ -131,18 +176,8 @@ class GeocodingLens(
       input.schema
            .fieldNames
            .map { field => col("_1").getField(field).as(field) } 
-        :+ when(array_size(coordinates) >= 2, element_at(coordinates, 1))
-             .otherwise(null).as(
-                 input.schema.fieldNames.foldLeft( 0 ){ (init, curr) => if( curr.asInstanceOf[String].startsWith(config.lat)) init+1 else init } match {
-                   case 0 => config.lat
-                   case colCnt => s"${config.lat}_${colCnt}"
-                 }) 
-        :+ when(array_size(coordinates) >= 2, element_at(coordinates, 2))
-             .otherwise(null).as(
-                 input.schema.fieldNames.foldLeft( 0 ){ (init, curr) => if( curr.asInstanceOf[String].startsWith(config.lon)) init+1 else init } match {
-                   case 0 => config.lon
-                   case colCnt => s"${config.lon}_${colCnt}"
-                 }) 
+        :+ buildLatOrLon(true) 
+        :+ buildLatOrLon(false)
       ):_*
 
     )
@@ -239,8 +274,17 @@ object TestCaseGeocoder extends Geocoder("TEST")
   {
     val rnd = new Random(Seq(house, street, city, state).mkString("; ").hashCode)
 
-    if(rnd.nextDouble < 0.9){
+    val target = rnd.nextDouble
+
+    if(target < 0.7){
       Seq(
+        rnd.nextDouble * 180 - 90, 
+        rnd.nextDouble * 360 - 180
+      )
+    } else if(target < 0.9){
+      Seq(
+        rnd.nextDouble * 180 - 90, 
+        rnd.nextDouble * 360 - 180,
         rnd.nextDouble * 180 - 90, 
         rnd.nextDouble * 360 - 180
       )

src/main/scala/org/mimirdb/util/LoggerUtils.scala
@@ -1,4 +1,4 @@
-package org.mimir.util
+package org.mimirdb.util
 
 import org.slf4j.{LoggerFactory};
 import ch.qos.logback.classic.{Level, Logger};

src/main/scala/org/mimirdb/util/StringUtils.scala
@@ -1,0 +1,14 @@
+package org.mimirdb.util
+
+object StringUtils
+{
+  def uniqueName(name: String, conflicts: Set[String]): String =
+  {
+    if(conflicts(name)){
+      name + "_" + (
+        (1 until conflicts.size + 1)
+          .find { x => !conflicts(s"${name}_${x}") }
+      )
+    } else { name }
+  }
+}

src/test/scala/org/mimirdb/api/SharedSparkTestInstance.scala
@@ -7,6 +7,8 @@ import org.mimirdb.lenses.implementation.TestCaseGeocoder
 import org.apache.spark.serializer.KryoSerializer
 import org.datasyslab.geosparkviz.core.Serde.GeoSparkVizKryoRegistrator
 import org.mimirdb.data.JDBCMetadataBackend
+import org.mimirdb.caveats.PrettyPrint
+
 
 object SharedSparkTestInstance
 {
@@ -50,6 +52,7 @@ object SharedSparkTestInstance
   def initAPI {
     this.synchronized {
       if(MimirAPI.sparkSession == null){
+        PrettyPrint.simpleOutput = true
         MimirAPI.sparkSession = spark
       }
       if(MimirAPI.conf == null){

src/test/scala/org/mimirdb/api/request/ExplainSpec.scala
@@ -4,11 +4,19 @@ import play.api.libs.json._
 import org.specs2.mutable.Specification
 import org.specs2.specification.BeforeAll
 
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.catalyst.plans.logical._
+import org.apache.spark.sql.catalyst.expressions._
+import org.apache.spark.sql.catalyst.encoders.RowEncoder
+
 import org.mimirdb.api.SharedSparkTestInstance
 import org.mimirdb.api.MimirAPI
 import org.mimirdb.rowids.AnnotateWithRowIds
-import org.mimir.util.LoggerUtils
+import org.mimirdb.util.LoggerUtils
 import org.mimirdb.lenses.Lenses
+import org.mimirdb.caveats.implicits._
+import org.mimirdb.caveats.EnumerableCaveatSet
 
 class ExplainSpec 
   extends Specification
@@ -73,18 +81,36 @@ class ExplainSpec
       val firstRowid = rows
           .head
           .getInt(0)
+      val secondRowid = rows
+          .tail
+          .head
+          .getInt(0)
       val lastRowid = rows
           .last
           .getInt(0)
-      //the first row LATITUDE should have a caveat message
+
+      df.showCaveats()
+
       val explainFirstRequest = ExplainCellRequest(
                 s"SELECT * FROM ${response.name}",
                 firstRowid.toString,
                 "LATITUDE"
               )
       val firstResult = Json.toJson(explainFirstRequest.handle).as[ExplainResponse]
-      firstResult.reasons must haveSize(1)
-      firstResult.reasons(0).message must not be null
+      firstResult.reasons must haveSize(0)
+
+      val explainSecondRequest = ExplainCellRequest(
+                s"SELECT * FROM ${response.name}",
+                secondRowid.toString,
+                "LATITUDE"
+              )
+      val secondResult = Json.toJson(explainSecondRequest.handle).as[ExplainResponse]
+      secondResult.reasons must haveSize(1)
+      secondResult.reasons(0).message must not be null
+
+
+
+
       //the last row LATITUDE (after a null in the middle) should have the same caveat message
       val explainLastRequest = ExplainCellRequest(
                 s"SELECT * FROM ${response.name}",

commit 5f657cab782758cbd07e107741375870a327a49f

Add to GeocodingLensSpec to illustrate new issue with AnnotateImplicitHeuristics introduced  by commit: 454b480a52168c139a2236cc52849ab0ef1f1fc9

src/test/scala/org/mimirdb/lenses/GeocodingLensSpec.scala
@@ -6,6 +6,11 @@ import org.specs2.mutable.Specification
 
 import org.mimirdb.api.{ MimirAPI, SharedSparkTestInstance }
 import org.mimirdb.lenses.implementation.TestCaseGeocoder
+import org.mimirdb.api.request.LoadRequest
+import org.mimirdb.api.request.CreateLensRequest
+import org.mimirdb.api.CreateResponse
+import org.mimirdb.api.request.CreateLensResponse
+import org.mimirdb.api.request.QueryTableRequest
 
 
 class GeocodingLensSpec 
@@ -63,6 +68,45 @@ class GeocodingLensSpec
 
     }
 
+    val loadRequest:LoadRequest = LoadRequest(
+                    file              = "test_data/geo.csv",
+                    format            = "csv",
+                    inferTypes        = true, 
+                    detectHeaders     = true,
+                    humanReadableName = Some("STILL MORE THING TESTS"),
+                    backendOption     = Seq(),
+                    dependencies      = Some(Seq()),
+                    resultName        = None,
+                    properties        = None,
+                    proposedSchema    = None
+                  )            
+    val loadResponse = Json.toJson(loadRequest.handle).as[CreateResponse]
+      
+    val lensRequest = CreateLensRequest(
+                      loadResponse.name,
+                      Json.obj(
+                        "houseColumn"  -> STRNUMBER,
+                        "streetColumn" -> STRNAME,
+                        "cityColumn"   -> CITY,
+                        "stateColumn"  -> STATE      
+                      ), 
+                      Lenses.geocode,
+                      false,
+                      Some("NULL TEST"),
+                      None,
+                      None
+                    )
+    val LensResponse = Json.toJson(lensRequest.handle).as[CreateLensResponse]
+     
+    val queryRequest =  QueryTableRequest(
+                            LensResponse.name,
+                            None,
+                            Some(25),
+                            Some(0),
+                            true,
+                            Some(true))
+    val queryResponse = Json.toJson(queryRequest.handle).as[CreateLensResponse]
+    
     ok
 
   }